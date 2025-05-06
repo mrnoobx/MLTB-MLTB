ddada877fae11eb9ce63b280017a93c20eca731a
mrnoobx <154804791+mrnoobx@users.noreply.github.com>
2025-05-06 23:19:52 +0530

    Update status_utils.py

--- bot/helper/ext_utils/status_utils.py
+++ bot/helper/ext_utils/status_utils.py
@@ -12,17 +12,18 @@ SIZE_UNITS = ["B", "KB", "MB", "GB", "TB", "PB"]
 
 
 class MirrorStatus:
-    STATUS_UPLOAD = "Upload"
-    STATUS_DOWNLOAD = "Download"
-    STATUS_CLONE = "Clone"
-    STATUS_QUEUEDL = "QueueDl"
-    STATUS_QUEUEUP = "QueueUp"
-    STATUS_PAUSED = "Pause"
-    STATUS_ARCHIVE = "Archive"
-    STATUS_EXTRACT = "Extract"
-    STATUS_SPLIT = "Split"
-    STATUS_CHECK = "CheckUp"
-    STATUS_SEED = "Seed"
+    STATUS_UPLOADING = "Uploading 📤"
+    STATUS_DOWNLOADING = "Downloading 📥"
+    STATUS_CLONING = "Cloning 🔃"
+    STATUS_QUEUEDL = "DL queued ⏳"
+    STATUS_QUEUEUP = "UL queued ⏳"
+    STATUS_PAUSED = "Paused ⛔️"
+    STATUS_ARCHIVING = "Archiving 🛠"
+    STATUS_EXTRACTING = "Extracting 📂"
+    STATUS_SPLITTING = "Splitting ✂️"
+    STATUS_CHECKING = "CheckUp ⏱"
+    STATUS_SEEDING = "Seeding 🌧"
+    STATUS_PROCESSING = "Processing ♻️"
     STATUS_SAMVID = "SamVid"
     STATUS_CONVERT = "Convert"
     STATUS_FFMPEG = "FFmpeg"
@@ -171,9 +172,9 @@ def get_progress_bar_string(pct):
         pct = float(pct.strip("%"))
     p = min(max(pct, 0), 100)
     c_full = int((p + 5) // 10)
-    p_str = "●" * c_full
-    p_str += "○" * (10 - c_full)
-    return p_str
+    p_str = "★" * cFull
+    p_str += "✩" * (10 - cFull)
+    return f"{p_str}"
 
 
 def source(self):
@@ -244,11 +245,11 @@ async def get_readable_message(sid, is_user, page_no=1, status="All", page_step=
                 with contextlib.suppress(Exception):
                     msg += f"\n<b>Seeders:</b> {task.seeders_num()} | <b>Leechers:</b> {task.leechers_num()}"
         elif tstatus == MirrorStatus.STATUS_SEED:
-            msg += f"\n<b>Size: </b>{task.size()}"
-            msg += f"\n<b>Speed: </b>{task.seed_speed()}"
-            msg += f"\n<b>Uploaded: </b>{task.uploaded_bytes()}"
-            msg += f"\n<b>Ratio: </b>{task.ratio()}"
-            msg += f" | <b>Time: </b>{task.seeding_time()}"
+            msg += f"\n<code>💽 Size</code>: {download.size()}"
+            msg += f"\n<code>🚀 Speed</code>: {download.upload_speed()}"
+            msg += f"\n<code>📈 Uploaded</code>: {download.uploaded_bytes()}"
+            msg += f"\n<code>📟 Ratio</code>: {download.ratio()}"
+            msg += f"\n<code>⏳ Time</code>: {download.seeding_time()}"
         else:
             msg += f"\n<b>Size: </b>{task.size()}"
         msg += f"\n<b>Tool:</b> {task.tool}"
